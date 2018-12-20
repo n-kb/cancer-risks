d64d5380f1aed219b52ef0f6f574ed5a231385f5
Nicolas Kayser-Bril <hi@nkb.fr>
2018-12-20 19:50:22 +0100

    added :de:

--- src/assets/texts.js
+++ src/assets/texts.js
@@ -24,16 +24,17 @@ const messages = {
         'hairdresser': 'Hairdresser (work as)'
      },
      cancers: {
-        "colorectum": "Colorectum",
-        "oral_cavity_and_pharynx": "Oral cavity and pharynx",
-        "larynx": "Larynx",
-        'lung': 'Lung',
-        'bladder': 'Bladder',
-        'lung_smoke': 'Lung',
-        'brain': 'Brain',
-        "oesophagus": "Oesophagus",
-        "female_breast": "Female breast",
-        "all": "All"
+        "colorectum": "colorectum",
+        "oral_cavity_and_pharynx": "oral cavity and pharynx",
+        "larynx": "larynx",
+        'lung': 'lung',
+        'bladder': 'bladder',
+        'lung_smoke': 'lung',
+        'brain': 'brain',
+        'pancreas': 'pancreas',
+        "oesophagus": "oesophagus",
+        "female_breast": "female breast",
+        "all": "all"
      },
      actions: {
         "drink50g": "drink 50g of pure alcohol (half a bottle of wine or a liter of beer) per day",
@@ -97,7 +98,86 @@ const messages = {
      'An interactive database by': 'Eine Datenbank von',
      'Share': 'Teilen',
      '20 December 2018': '20. Dezember 2018',
-     'Cancer Risks': 'Krebsrisiko'
+     'Cancer Risks': 'Krebsrisiko',
+     'Cancer site': 'Art des Krebses',
+     'Risk factor': 'Risikofaktoren',
+     share_text: 'Krebsrisikofaktoren einfach und klar vergleichen.',
+     factors: {
+        'alcohol': 'Alkohol',
+        'aluminum_deo': 'Aluminum (Deo)',
+        'processed_meat': 'Industriell verarbeitetes Fleisch',
+        'red_meat': 'Rotes Fleisch',
+        'coffee': 'Kaffee',
+        'air_pollution': 'Luftverschmutzung',
+        'mobile_phones': 'Handys',
+        'cig_smoke': 'Rauchen',
+        'hairdresser': 'Friseur (Job)'
+     },
+     cancers: {
+        "colorectum": "Dickdarm",
+        "oral_cavity_and_pharynx": "Rachen und Mund",
+        "larynx": "Kehlkopf",
+        'lung': 'Lunge',
+        'bladder': 'Blase',
+        'lung_smoke': 'Lunge',
+        'brain': 'Gehirn',
+        'pancreas': 'Bauchspeicheldrüse',
+        "oesophagus": "Speiseröhre",
+        "female_breast": "Weibliche Brust",
+        "all": "Alle"
+     },
+     actions: {
+        "drink50g": "50 Gramm pures Alkohols (etwa 1 Liter Bier) pro Tag trinken",
+        "useDeodorant": "jeden Tag ein Aluminum-Deo nutzen",
+        "eat50gprocessedmeat": "jeden Tag 50 Gramm verarbeitetes Fleisch essen",
+        "eat100gredmeat": "jeden Tag 100 Gramm rotes Fleisch essen",
+        "drinkCoffee": "jeden Tag Kaffee trinken",
+        "live20mug": "in einer Stadt leben, wo die PM<sub>2.5</sub>-Konzentration einen Jahresdurchschnitt von 20μg/m<sup>3</sup> überschreitet (entspricht die Luftverschmutzung Berlins)",
+        "cellphone": "jeden Tag weniger als 30 Minuten ihr Handy am Ohr haben",
+        'smoke': 'ungefähr 20 Zigaretten pro Tag rauchen',
+        'work_as_hairdresser': 'mindestens 10 Jahren als Friseur gearbeitet haben'
+     },
+     comparator: {
+        source: `Quelle: {source} <a href="{source_url}" target="_blank">Link</a>.`,
+        disclaimer: "Wissenschaftler haben mehrere Studie zum Thema geführt und konnten keinen Zusammenhang zwischen Krebs und diesem Faktor finden. ",
+        nonsmokers: "Nichtraucher",
+        males: "Männer",
+        females: "Frauen",
+        persons: "Personnen",
+        a_cancer: "Krebs",
+        cancer_type: `{cancer}-Krebs`,
+        sentence_right: `Von {amount} {persons}, die {action}, werden <span class="big-number">{rate}</span> im Zuge ihres Lebens {cancer_type} haben.`,
+        sentence_left: `Von {amount} {persons} der Gesamtbevölkerung, werden <span class="big-number">{rate}</span> im Zuge ihres Lebens {cancer_type} haben.`
+     },
+     copy: {
+        main: `
+        <p>
+          Aspirin <a href="https://www.dailymail.co.uk/health/article-205490/Asprin-link-cancer-risk.html" target="_blank">ist krebserregend</a>, schrieb eines Tages die <em>Daily Mail</em>. Aspirin sei auch <a href="https://www.dailymail.co.uk/health/article-1174691/Taking-aspirin-40s-cuts-cancer-risk.html" target="_blank">krebsschützend</a>, schrieb die eines anderen Tages. Zwiebel <a href="https://www.dailymail.co.uk/health/article-207275/Onions-fight-prostate-cancer.html" target="_blank">vermindern</a> das Krebsrisiko und künstliches Licht <a href="https://www.dailymail.co.uk/health/article-391267/Artificial-light-increases-breast-cancer-risk.html" target="_blank">erhöht</a> es.
+        </p>
+        <p>
+          Diese Beispiele sind aus der geniale <a href="http://kill-or-cure.herokuapp.com" target="_blank">Kill or Cure</a> entnommen, eine Liste von allem, was die <em>Daily Mail</em> als krebserregend oder krebsschützend (oder beides) jemals eingestüft hat. Dass die <em>Mail</em> schwachen Journalismus produziert ist keine Neuheit, aber auch gute Zeitungen können manchmal irreführende Beiträge veröffentlichen, besonders wenn es um Krebsrisiken geht.
+        </p>
+        <p>
+          Viele Journalisten verstehen wissenschaftliche Artikel nur kaum. Andererseits verschönen viele PR-Abteilungen von Universitäten ihre Pressemitteilungen, damit eine Studie mehr Aufmerksamkeit bekommt, als sie es Wert ist. Es ist am Ende sehr schwierig, einen einfachen Zugang zum Wissenschaftszustand im Bereich Krebsrisikofaktoren zu bekommen.</p>
+        <img src="https://blog.nkb.fr/cancer-risks/graph_de.png" />
+        <p>
+          Mehrere Gruppen von Wissenschaftlern sammeln und evaluieren Studien zu diesem Thema, insbesondere die Internationale Agentur für Krebsforschung. Ihre Monographien sind zwar sehr umfassend, aber nicht immer leicht zu lesen. Ich habe einige gelesen und die Daten zu den Krebsrisikofaktoren extrahiert. Die Zahlen, die unten zu lesen sind, sind nicht definitiv (der Grund eines Krebses genau zu identifizieren ist extrem schwierig) aber derer Größenordnungen sind wahrscheinlich informativer als die Schlagzeilen einiger Zeitungen.
+        </p>
+        <p>
+          Wählen Sie ein Risikofaktor und eine Art von Krebs und sehen Sie, wie das absolute Risiko sich ändert.
+        </p>`,
+        limitations: `
+        <h2>Einschränkungen</h2>
+        <p>
+          Die Daten zum Krebsrisiko in der Gesamtbevölkerung kommen aus den Vereinigten Staaten. Die Lage in Ihrem Land kann anders sein.
+        </p>
+        <p>
+          Dass keinen Zusammenhang zwischen einem Risikofaktor und einem Krebs gefunden war, heißt nicht, dass keinen existiert. (Es ist sowieso logisch ausgesehen unmöglich, eine solche Abwesenheit zu beweisen). Vielleicht wird irgendwann eine Studie einen solchen Zusammenhang beweisen. Die Beispiele in diesem Artikel wurden aber alle von mehreren Studien geforscht und keine davon könnte einen Zusammenhang beweisen.
+        </p>
+        <p>
+          Da ich kein Onkologe bin, habe ich Fehler machen können. Die Daten, die ich benutzt habe, sind <a href="https://docs.google.com/spreadsheets/d/1QTflR63InxdWNeJGgpYgnd7rCC_r7SzZlOTiyGnkQHw/edit?usp=sharing">in dieser Tabelle zu finden</a>. Schicken Sie mir eine Nachricht an hi@nkb.fr falls etwas Ihnen stört, oder wenn Sie ein anderes Risikofaktor hinzufügen möchten.
+        </p>`
+     }
     },
   fr: {
      lede: 'Comparez les risques de cancer facilement et clairement. {risks} facteurs de risque sont disponibles pour l\'instant.',
@@ -124,22 +204,23 @@ const messages = {
         'hairdresser': 'Coiffeur (profession)'
      },
      cancers: {
-        "colorectum": "Côlon",
-        "oral_cavity_and_pharynx": "Pharynx et bouche",
-        "larynx": "Larynx",
-        'lung': 'Poumon',
-        'bladder': 'Vessie',
-        'lung_smoke': 'Poumon',
-        'brain': 'Cerveau',
-        "oesophagus": "Œsophage",
-        "female_breast": "Sein féminin",
-        "all": "Tous"
+        "colorectum": "côlon",
+        "oral_cavity_and_pharynx": "pharynx et bouche",
+        "larynx": "larynx",
+        'lung': 'poumon',
+        'bladder': 'vessie',
+        'lung_smoke': 'poumon',
+        'brain': 'cerveau',
+        'pancreas': 'pancréas',
+        "oesophagus": "œsophage",
+        "female_breast": "sein féminin",
+        "all": "tous"
      },
      actions: {
         "drink50g": "boivent 50g d'alcool pur (une demie bouteille de vin ou un litre de bière) par jour",
         "useDeodorant": "utilisent un déodorant à base d'aluminium tous les jours",
         "eat50gprocessedmeat": "mangent 50g de viande transformée tous les jours",
-        "eat100gredmeat": "mangent 50g de viande rouge tous les jours",
+        "eat100gredmeat": "mangent 100g de viande rouge tous les jours",
         "drinkCoffee": "boivent du café tous les jours",
         "live20mug": "vivent dans une ville où la concentration en PM<sub>2.5</sub> dépasse 20μg/m<sup>3</sup> (un peu plus que la moyenne annuelle à Paris)",
         "cellphone": "ont un téléphone à l'oreille moins de 30 minutes par jour",
